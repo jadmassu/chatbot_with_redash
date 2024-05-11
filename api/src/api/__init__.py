d9a09b309ec42d0b3bb7d44879a40e22be83df5f
eyerusalem <eyerusad12@gmail.com>
2024-05-11 18:46:18 +0300

    ChatPromptTemplate fixed

--- api/src/api/__init__.py
+++ api/src/api/__init__.py
@@ -21,16 +21,21 @@ def run() -> None:
     app.run()
     
 
-@app.post("/getUserInput")
+@app.post("/getQuery")
 async def userInput():
-    # return True
-   prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are sql query generator."),
-    ("user", "{input}")
-])
-   output_parser = StrOutputParser()
-   chain = prompt | llm  | output_parser
-   return chain.invoke({"input": "give me all users based on all city"})
+    message = await request.get_json()
+    print("mesaf",message.get("query"))
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are an SQL query generator. you can produce SQL queries without prior knowledge of specific table information. You will generate SQL queries based on each input provided to you, identifying words that could be a name of a table. If necessary information is missing or insufficient to generate a valid SQL query, you will return None."),
+        ("user", "{input}")
+    ])
+    output_parser = StrOutputParser()
+    chain = prompt | llm  | output_parser
+    return chain.invoke({"input": "select all users and order them "})
 
-
+@app.post("/echo")
+async def echo():
+    data =  await request.get_json()
+    print(data)
+    return {"input": data, "extra": True}
 
